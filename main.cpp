565c4736d424ffc230f661d63e03612f2dcab609
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-09-30 16:57:21 +0300

    Done Task 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+
+//---------------------------------------------------------------------------------
+//  Task 1. Create an application that displays information 
+//  about the book in the following format :
+//  Name        : “The war and the peace”
+//  Avtor       : L.N.Tolstoj
+//  Izdatelstvo : Piter
+//  Pages       : 500.
+//---------------------------------------------------------------------------------
 #include <iostream>
 using namespace std;
 
 int main() 
 {
-	return 0;
+    cout << "Name       : \"The warand the peace\"" << endl;
+    cout << "Avtor      : L.N.Tolstoj" << endl;
+    cout << "Izdatelstvo: Piter" << endl;
+    cout << "Pages      : 500." << endl;
+    return 0;
 }
