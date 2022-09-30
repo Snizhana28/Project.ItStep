affc7a921de13a671e8e93d45ccf1a67dbb5deb1
snizhana28 <snizhana2812kostyuk@gmail.com>
2022-09-30 19:13:51 +0300

    Done Task 3

--- main.cpp
+++ main.cpp
@@ -3,5 +3,39 @@ using namespace std;
 
 int main() 
 {
+	cout << "\n"
+		<< (char)201
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)187 << "\n" << endl;
+	cout << (char)186 << "                       Pory roku                       " << (char)186 << "\n" << endl;
+	cout << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 
+		<< (char)185 << endl;
+	cout << "\n" << (char)186 << "    Zyma     " << (char)186 << "    Vesna    " << (char)186 << "    Lito     " << (char)186 << "    Osin     " << (char)186 << endl;
+	cout << "\n" << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202
+        << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)188 << endl;
 	return 0;
 }
